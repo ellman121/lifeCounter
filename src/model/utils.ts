3f714a3092a3e4629d74d034e52ea9968fdde6b6
Elliott Rarden <ellman121@gmail.com>
2022-07-21 22:18:20 +0200

    Nicer life array handling

--- src/model/utils.ts
+++ src/model/utils.ts
@@ -1,5 +1,5 @@
 import R from 'ramda'
 
 export function shiftArray<T = any>(list: T[], n: T, length: number = 5) {
-  return R.append(n, R.takeLast(4, list))
+  return R.prepend(n, R.take(length - 1, list))
 }
